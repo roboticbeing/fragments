e6cb21c43f79224aeefaabbcdbb3a23db4c53b6d
Jessica <jessicakrishtul@gmail.com>
2022-09-17 18:42:48 -0400

    Add new GET api route

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const helmet = require('helmet');
 const compression = require('compression');
 
 // version and author from our package.json file
-const { version, author } = require('../package.json');
+//commented out because of unused variables
+//const { version, author } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -29,21 +30,7 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Define a simple health check route. If the server is running
-// we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/roboticbeing/fragments',
-    version,
-  });
-});
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
