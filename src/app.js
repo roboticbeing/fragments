74e74aca23753ea250bd93f12514125dcc277044
Jessica <jessicakrishtul@gmail.com>
2022-09-17 18:48:59 -0400

    Add authorization

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
+const authorization = require('./authorization');
 
 // version and author from our package.json file
 //commented out because of unused variables
@@ -30,6 +32,11 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
+// Set up our passport authorization middleware
+passport.use(authorization.strategy());
+app.use(passport.initialize());
+
+// Define our routes
 app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
