f83b17541a346bf6cae2e07cad6b0decdf52c20d
Jessica <jessicakrishtul@gmail.com>
2022-10-08 15:48:06 -0400

    implement post route

---
+++ src/routes/api/post.js
@@ -0,0 +1,27 @@
+// src/routes/api/post.js
+
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+
+/**
+ * Get a list of fragments for the current user
+ */
+ module.exports = (req, res) => {
+    try {
+        const data = req.body;
+        const fragment = new Fragment({ ownerId: req.user, type: req.get('content-type')});
+        fragment.save();
+        fragment.setData(data);
+        res.setHeader('Location', 'localhost:8080/v1'+req.url)
+        const successResponse = createSuccessResponse(fragment);
+        res.status(201).json(
+        successResponse
+        );
+    }
+    catch (err) {
+        const errorResponse = createErrorResponse(415, 'unsupported content-type');
+        res.status(errorResponse.error.code).json({
+            errorResponse
+          });
+        }
+  };
