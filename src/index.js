2c03d989195e77bdcfc5c4405796cde075ee3c16
Jessica <jessicakrishtul@gmail.com>
2022-10-08 17:44:20 -0400

    add different environment configurations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 
 // Read environment variables from an .env file (if present)
-require('dotenv').config();
+require('dotenv').config({ path: `env.${process.env.NODE_ENV}` })
 
 // We want to log any crash cases so we can debug later from logs.
 const logger = require('./logger');
