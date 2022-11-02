61d2ce44c9878fa4dd9d3736d6b7298a71bb6a2b
Jessica <jessicakrishtul@gmail.com>
2022-11-02 19:59:40 -0400

    Modify Get Route to include expand

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,12 +1,14 @@
 // src/routes/api/get.js
 
 const { createSuccessResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
 
 /**
  * Get a list of fragments for the current user
  */
- module.exports = (req, res) => {
-    const data = { fragments: []}
+ module.exports = async (req, res) => {
+    const fragment = await Fragment.byUser(req.user, req.query.expand);
+    const data = { fragments: fragment}
     const successResponse = createSuccessResponse(data);
     res.status(200).json(
       successResponse
